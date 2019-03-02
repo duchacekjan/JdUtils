cbd76299b70255636467efa6c2142dd8ba5281e0
JD <jan.duchacek@fullsys.cz>
2019-03-02 16:09:05 +0100

    Created delegates for background worker for logging and error. Initialization of background worker with ui dispatcher. Created method for posting to ui and handling default states. Created method wrapping calling working method and error handlig for methods without parameters

---
+++ JdUtils/BackgroundWorker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using JdUtils.Delegates;
+
+namespace JdUtils
+{
+    public class BackgroundWorker
+    {
+        private readonly Dispatcher m_uiDispatcher;
+
+        public BackgroundWorker(Dispatcher uiDispatcher)
+        {
+            if (uiDispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(uiDispatcher));
+            }
+
+            if (uiDispatcher.Thread.GetApartmentState() != ApartmentState.STA)
+            {
+                throw new ArgumentOutOfRangeException(nameof(uiDispatcher), "Dispatcher is not from STA thread");
+            }
+
+            m_uiDispatcher = uiDispatcher;
+        }
+
+        public event ErrorHandler Error;
+
+        public event LogHandler Log;
+        
+        private async Task ExecuteSafe(Func<Task> worker, Action success = null, Action<Exception> failure = null)
+        {
+            try
+            {
+                await worker.Invoke();
+                PostToUi(() =>
+                {
+                    success?.Invoke();
+                });
+            }
+            catch (Exception e)
+            {
+                Log?.Invoke(e.Message, e);
+                PostToUi(() =>
+                {
+                    if (failure == null)
+                    {
+                        Error?.Invoke(e);
+                    }
+                    else
+                    {
+                        failure.Invoke(e);
+                    }
+                });
+            }
+        }
+        
+        private void PostToUi(Action action)
+        {
+            if (m_uiDispatcher.CheckAccess())
+            {
+                action?.Invoke();
+            }
+            else
+            {
+                m_uiDispatcher.Invoke(action);
+            }
+        }
+    }
+}
