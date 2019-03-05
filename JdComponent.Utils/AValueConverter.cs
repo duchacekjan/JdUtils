cd8f3bb858b4e887c94321b67ddca11037884617
JD <jan.duchacek@fullsys.cz>
2019-03-05 07:46:55 +0100

    Created utils project for components. Added base value conveter

---
+++ JdComponent.Utils/AValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace JdComponent.Utils
+{
+    /// <summary>
+    /// Common base for value converters with support of <see cref="MarkupExtension"/>
+    /// </summary>
+    [MarkupExtensionReturnType(typeof(IValueConverter))]
+    public abstract class AValueConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public abstract object Convert(object value, Type targetType, object parameter, CultureInfo culture);
+
+        public abstract object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture);
+    }
+}
