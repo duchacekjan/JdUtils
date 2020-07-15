13135caa95ff3c63834f7670914534f364e54526
JD <jan.duchacek.bc@gmail.com>
2020-07-15 12:16:02 +0200

    Created ratio converter

---
+++ JdUtils/Converters/RatioConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace JdUtils.Converters
+{
+    public class RatioConverter : AValueConverter
+    {
+        public double Ratio { get; set; } = 1;
+
+        public double MinValue { get; set; } = double.NegativeInfinity;
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = DependencyProperty.UnsetValue;
+            if (value is double number)
+            {
+                var newNumber = number * Ratio;
+                if (newNumber < MinValue)
+                {
+                    newNumber = MinValue;
+                }
+                result = newNumber;
+            }
+            return result;
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
