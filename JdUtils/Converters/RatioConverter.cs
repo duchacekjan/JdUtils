c8fc9f36f382fbb70ab98b425fb381de5837ebaa
JD <jan.duchacek.bc@gmail.com>
2020-07-16 10:36:50 +0200

    Přidána podpora do RatioConverteru na změnu thickness

--- JdUtils/Converters/RatioConverter.cs
+++ JdUtils/Converters/RatioConverter.cs
@@ -13,21 +13,45 @@ namespace JdUtils.Converters
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var result = DependencyProperty.UnsetValue;
-            if (value is double number)
+            switch (value)
             {
-                var newNumber = number * Ratio;
-                if (newNumber < MinValue)
-                {
-                    newNumber = MinValue;
-                }
-                result = newNumber;
+                case double number:
+                    result = ConvertDouble(number);
+                    break;
+                case Thickness thickness:
+                    result = ConvertThickness(thickness);
+                    break;
             }
+
             return result;
         }
 
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return DependencyProperty.UnsetValue;
+        }
+
+        private double ConvertDouble(double number)
+        {
+            var newNumber = number * Ratio;
+            if (newNumber < MinValue)
+            {
+                newNumber = MinValue;
+            }
+            return newNumber;
+        }
+
+        private Thickness ConvertThickness(Thickness thickness)
+        {
+            var newLeft = ConvertDouble(thickness.Left);
+            thickness.Left = newLeft;
+            var newTop = ConvertDouble(thickness.Top);
+            thickness.Top = newTop;
+            var newRight = ConvertDouble(thickness.Right);
+            thickness.Right = newRight;
+            var newBottom = ConvertDouble(thickness.Bottom);
+            thickness.Bottom = newBottom;
+            return thickness;
         }
     }
 }
