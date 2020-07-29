ca356e32d13e4ab8fd11d3e6a06d592b6f6c894a
JD <jan.duchacek.bc@gmail.com>
2020-07-29 08:34:48 +0200

    Oprava podmínky v converteru

--- JdUtils/Converters/EnumConverter.cs
+++ JdUtils/Converters/EnumConverter.cs
@@ -9,7 +9,7 @@ namespace JdUtils.Converters
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var result = DependencyProperty.UnsetValue;
-            if (value != null && targetType != null)
+            if (value != null && targetType != null && targetType.IsEnum)
             {
                 var numValue = System.Convert.ToInt64(value);
                 result = Enum.ToObject(targetType, numValue);
