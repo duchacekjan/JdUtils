420c0380db21ebef84accd13dbf4e32d4d7f1c32
JD <jan.duchacek.bc@gmail.com>
2020-07-29 08:33:33 +0200

    Vytvořen UrlImage markup extension to show image from url

---
+++ JdUtils/MarkupExtensions/UrlImage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace JdUtils.MarkupExtensions
+{
+    [MarkupExtensionReturnType(typeof(ImageSource))]
+    public class UrlImage : MarkupExtension
+    {
+        public string ImageLink { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            ImageSource result = null;
+            if (!string.IsNullOrEmpty(ImageLink))
+            {
+                var bitmap = new BitmapImage();
+                try
+                {
+                    bitmap.BeginInit();
+                    bitmap.UriSource = new Uri(ImageLink, UriKind.Absolute);
+                    bitmap.EndInit();
+                    result = bitmap;
+                }
+                catch {}
+            }
+
+            return result;
+        }
+    }
+}
