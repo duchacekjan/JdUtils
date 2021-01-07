77358455d6b35a474c995aed28a64736355f3ebf
JGS <jan.duchacek.bc@gmail.com>
2021-01-07 08:24:51 +0100

    Vytvořen backgound worker builder. Created alpha01 verze nugetu

---
+++ JdUtils/ABackgroundWorker.cs
@@ -0,0 +1,132 @@
+﻿using JdUtils.Delegates;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace JdUtils
+{
+    public abstract class ABackgroundWorker
+    {
+        /// <summary>
+        /// Constructor. Creates new instance of <see cref="BackgroundWorker"/>
+        /// </summary>
+        /// <param name="uiDispatcher"><see cref="Dispatcher"/> from UI thread</param>
+        protected ABackgroundWorker(Dispatcher uiDispatcher)
+        {
+            if (uiDispatcher == null)
+            {
+                throw new ArgumentNullException(nameof(uiDispatcher));
+            }
+
+            if (uiDispatcher.Thread.GetApartmentState() != ApartmentState.STA)
+            {
+                throw new ArgumentOutOfRangeException(nameof(uiDispatcher), "Dispatcher is not from STA thread");
+            }
+
+            Dispatcher = uiDispatcher;
+        }
+
+        /// <summary>
+        /// Event invoked when unhandled error occured
+        /// </summary>
+        public event ExceptionHandler UnhandledError;
+
+        /// <summary>
+        /// Event invoked when error occured
+        /// </summary>
+        public event ExceptionHandler LogError;
+
+        protected Dispatcher Dispatcher { get; }
+
+        /// <summary>
+        /// Invokes action on UI thread
+        /// </summary>
+        /// <param name="action"></param>
+        /// <param name="parameter"></param>
+        public void PostToUi<T>(Action<T> action, T parameter)
+        {
+            void Wrapper()
+            {
+                action?.Invoke(parameter);
+            }
+
+            PostToUi(Wrapper);
+        }
+
+        /// <summary>
+        /// Invokes action on UI thread
+        /// </summary>
+        /// <param name="action"></param>
+        public void PostToUi(Action action)
+        {
+            try
+            {
+                if (Dispatcher.CheckAccess())
+                {
+                    action?.Invoke();
+                }
+                else
+                {
+                    Dispatcher.Invoke(action);
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                ;//INFO Silent handling task cancellation
+            }
+        }
+
+        /// <summary>
+        /// Executes action <paramref name="worker"/> on background thread wrapped in
+        /// try/catch clause. If <paramref name="delay"/> is 0 is invoked Task.CompletedTask,
+        /// otherwise is invoked Task.Delay
+        /// </summary>
+        /// <param name="worker">Action to be executed on background thread</param>
+        /// <param name="failure">Custom error handler</param>
+        /// <param name="delay">Delay in milliseconds</param>
+        /// <exception cref="ArgumentOutOfRangeException">Negative delay</exception>
+        protected void ExecuteSafe(Action worker, ExceptionHandler failure, int delay = 0)
+        {
+            Task.Run(async () =>
+            {
+                try
+                {
+                    if (delay > 0)
+                    {
+                        await Task.Delay(delay);
+                    }
+                    else
+                    {
+                        await Task.CompletedTask;
+                    }
+
+                    worker?.Invoke();
+                }
+                catch (Exception e)
+                {
+                    OnException(e, failure);
+                }
+            });
+        }
+
+        /// <summary>
+        /// Hadling error on background thread. Method invokes <see cref="LogError"/> event
+        /// and if is not defined custom handler (<paramref name="failure"/>), invokes <see cref="UnhandledError"/>
+        /// </summary>
+        /// <param name="exception">Occured exception</param>
+        /// <param name="failure">Custom handler</param>
+        private void OnException(Exception exception, ExceptionHandler failure)
+        {
+            LogError?.Invoke(exception);
+            if (failure != null)
+            {
+                PostToUi(failure.Invoke, exception);
+            }
+            else if (UnhandledError != null)
+            {
+                PostToUi(UnhandledError.Invoke, exception);
+            }
+        }
+    }
+}
---
+++ JdUtils/BackgroundWorkerBuilder.cs
@@ -0,0 +1,154 @@
+﻿using JdUtils.Delegates;
+using System;
+using System.Windows.Threading;
+
+namespace JdUtils
+{
+    public class BackgroundWorkerBuilder : ABackgroundWorker
+    {
+        private Action m_worker;
+        private ExceptionHandler m_failure;
+        private int m_delay;
+
+        public BackgroundWorkerBuilder(Dispatcher uiDispatcher)
+            : base(uiDispatcher)
+        {
+        }
+
+        private Action Worker
+        {
+            get => m_worker;
+            set => SetWorker(value);
+        }
+
+        private void SetWorker(Action value)
+        {
+            m_delay = 0;
+            m_failure = null;
+            m_worker = value;
+        }
+
+        /// <summary>
+        /// Runs execution
+        /// </summary>
+        public void Execute()
+        {
+            ExecuteSafe(Worker, m_failure, m_delay);
+        }
+
+        /// <summary>
+        /// Executes data on background with defined delay. Values 0 or less means no delay
+        /// </summary>
+        /// <param name="delay"></param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder WithDelay(int delay)
+        {
+            m_delay = delay;
+            return this;
+        }
+
+        /// <summary>
+        /// Action when execution fails. When not assigned, <see cref="UnhandledError"/> is invoked.
+        /// </summary>
+        /// <param name="failure"></param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder OnException(ExceptionHandler failure)
+        {
+            m_failure = failure;
+            return this;
+        }
+
+        /// <summary>
+        /// Creates wrapping action to handle <paramref name="worker"/> on background thread and
+        /// then synchronize <paramref name="success"/> on UI thread
+        /// </summary>
+        /// <typeparam name="T">Type of function parameter</typeparam>
+        /// <typeparam name="TResult">Type of returning value</typeparam>
+        /// <param name="worker">Function to be executed on background thread</param>
+        /// <param name="param">Parameter of function</param>
+        /// <param name="success">Action to be executed on UI thread, when <paramref name="worker"/> action is successful</param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder DoWork<T, TResult>(Func<T, TResult> worker, T param, Action<TResult> success)
+        {
+            Worker = () =>
+            {
+                var result = worker.Invoke(param);
+                PostToUi(success, result);
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Creates wrapping action to handle <paramref name="worker"/> on background thread and
+        /// then synchronize <paramref name="success"/> on UI thread
+        /// </summary>
+        /// <typeparam name="T">Type of returning value</typeparam>
+        /// <param name="worker">Function to be executed on background thread</param>
+        /// <param name="success">Action to be executed on UI thread, when <paramref name="worker"/> action is successful</param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder DoWork<T>(Func<T> worker, Action<T> success)
+        {
+            Worker = () =>
+            {
+                var result = worker.Invoke();
+                PostToUi(success, result);
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Creates wrapping action to handle <paramref name="worker"/> on background thread and
+        /// then synchronize <paramref name="success"/> on UI thread
+        /// </summary>
+        /// <typeparam name="T">Type of returning value</typeparam>
+        /// <param name="worker">Function to be executed on background thread</param>
+        /// <param name="param">Parameter of action</param>
+        /// <param name="success">Action to be executed on UI thread, when <paramref name="worker"/> action is successful</param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder DoWork<T>(Action<T> worker, T param, Action success)
+        {
+            Worker = () =>
+            {
+                worker.Invoke(param);
+                PostToUi(success);
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Creates wrapping action to handle <paramref name="worker"/> on background thread and
+        /// then synchronize <paramref name="success"/> on UI thread
+        /// </summary>
+        /// <param name="worker">Action to be executed on background thread</param>
+        /// <param name="success">Action to be executed on UI thread, when <paramref name="worker"/> action is successful</param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder DoWork(Action worker, Action success)
+        {
+            Worker = () =>
+            {
+                worker.Invoke();
+                PostToUi(success);
+            };
+
+            return this;
+        }
+
+        /// <summary>
+        /// Calls action on UI thread
+        /// </summary>
+        /// <param name="success">Action to be executed back on UI thread</param>
+        /// <returns></returns>
+        public BackgroundWorkerBuilder AfterDelay(Action success)
+        {
+            Worker = () =>
+            {
+                PostToUi(success);
+            };
+
+            return this;
+        }
+    }
+}
